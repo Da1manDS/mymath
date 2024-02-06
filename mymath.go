190567ccfefbcc37c73a9aff5ef845c67fe4674f
Da1manDS <da1manskiydiavol@gmail.com>
2024-02-06 22:20:14 +0300

    add any func

--- mymath.go
+++ mymath.go
@@ -8,6 +8,18 @@ func Abs(x float64) float64 {
 	return math.Abs(x)
 }
 
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
